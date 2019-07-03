d313700adde9bf17c58f990f419b6266bb9a5f07
Ivan F <pytnik89@mail.ru>
2019-07-03 22:29:31 +0300

    added some first steps

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log('Hello world!')
+const FIO = 'Vasa Pupkin'
+const number = 1
+const string = '1'
+const boolean = true
+const unknown = undefined
+const Null = null
+const Fun = function () {
+}
+const obj = {
+  xxx: FIO,
+  FIO: FIO,
+  x: number,
+  y: string,
+  boolean,
+  unknown
+}
+const arr = [number, obj, string, boolean]
+
+console.log(obj)
